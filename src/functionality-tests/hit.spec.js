78a13431a2124ec941a5b11d69c09acf51881968
Shalama <shalamafadillaa@gmail.com>
2026-01-19 14:10:27 +0700

    Add describe block for hit function tests

--- src/functionality-tests/hit.spec.js
+++ src/functionality-tests/hit.spec.js
@@ -3,10 +3,12 @@ import { Ship } from "../functionality/ship.js";
 
 const ship = new Ship(3);
 
-test('hit function is defined', () => {
-    expect(hit).toBeDefined();
-})
+describe('hit function', () => {
+    test('hit function is defined', () => {
+        expect(hit).toBeDefined();
+    })
 
-test('make hit function returns 1', () => {
-    expect(hit()).toBe(1);
+    test('make hit function returns 1', () => {
+        expect(hit()).toBe(1);
+    })
 })
