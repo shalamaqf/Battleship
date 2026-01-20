2260109e7f890d24199260a568e70ca45c913423
Shalama <shalamafadillaa@gmail.com>
2026-01-20 13:18:30 +0700

    Make isAvailCoordinate method returns false
    Returns false when the argument is {x: 0, y: 1}.

--- src/functionality-tests/isAvailCoordinate.spec.js
+++ src/functionality-tests/isAvailCoordinate.spec.js
@@ -2,6 +2,13 @@ import { GameBoard } from "../functionality/game-board.js";
 
 const board = new GameBoard();
 
-test('isAvailCoordinate method is defined', () => {
-    expect(board.isAvailCoordinate).toBeDefined();
+describe('isAvailCoordinate method', () => {
+    test('isAvailCoordinate method is defined', () => {
+        expect(board.isAvailCoordinate).toBeDefined();
+    });
+
+    test('returns true when coordinate object {0, 1}', () => {
+        expect(board.isAvailCoordinate({x: 0, y: 1})).toBe(false);
+    })
 });
+
--- src/functionality/game-board.js
+++ src/functionality/game-board.js
@@ -6,6 +6,6 @@ export class GameBoard {
     }
 
     isAvailCoordinate({x, y}) {
-
+        return false;
     }
 }
