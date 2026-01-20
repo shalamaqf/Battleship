5f331d523fb51af8659e46bf0ee98dbbe73d9c9e
Shalama <shalamafadillaa@gmail.com>
2026-01-20 13:45:22 +0700

    Make placeShip method returns false
    Returns false with given incorrect coordinate object.

--- src/functionality-tests/placeShip.spec.js
+++ src/functionality-tests/placeShip.spec.js
@@ -6,4 +6,8 @@ describe('placeShip method', () => {
     test('placeShip method is defined', () => {
         expect(board.placeShip).toBeDefined();
     })
+
+    test('placeShip method returns false with incorrect coordinate', () => {
+        expect(board.placeShip({x: 0, y: 9})).toBe(false);
+    })
 })
--- src/functionality/game-board.js
+++ src/functionality/game-board.js
@@ -11,7 +11,7 @@ export class GameBoard {
         return true;
     }
 
-    placeShip() {
-        
+    placeShip({x, y}) {
+        return false;
     }
 }
