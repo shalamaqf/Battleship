edb65ce58c4f70ac8477c356e4d057b6df524ab9
Shalama <shalamafadillaa@gmail.com>
2026-01-21 14:55:55 +0700

    Make areAllShipsSunk returns true
    Return true when all ships on the board has sunk.

--- src/functionality-tests/areAllShipsSunk.spec.js
+++ src/functionality-tests/areAllShipsSunk.spec.js
@@ -23,4 +23,33 @@ describe('arrAllShipsSunk method', () => {
 
         expect(board.areAllShipsSunk()).toBe(false);
     })
+
+    test('return true when all ships has sunk', () => {
+        const board = new GameBoard();
+        board.placeShip({x: 2, y: 3}, 4, Direction.HORIZONTAL);
+        board.placeShip({x: 4, y: 1}, 2, Direction.VERTICAL);
+        board.placeShip({x: 7, y: 5}, 3, Direction.VERTICAL);
+        board.placeShip({x: 3, y: 5}, 2, Direction.HORIZONTAL);
+
+        // First ship hits
+        board.receiveAttack({x: 2, y: 3});
+        board.receiveAttack({x: 3, y: 3});
+        board.receiveAttack({x: 4, y: 3});
+        board.receiveAttack({x: 5, y: 3});
+
+        // Second ship hits
+        board.receiveAttack({x: 4, y: 1});
+        board.receiveAttack({x: 4, y: 2});
+
+        // Third ship hits
+        board.receiveAttack({x: 7, y: 5});
+        board.receiveAttack({x: 7, y: 6});
+        board.receiveAttack({x: 7, y: 7});
+
+        // Fourth ship hits
+        board.receiveAttack({x: 3, y: 5});
+        board.receiveAttack({x: 4, y: 5});
+
+        expect(board.areAllShipsSunk()).toBe(true);
+    })
 })
