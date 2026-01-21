5c74dba0fddfca66c83abc8f67a2165f406df87e
Shalama <shalamafadillaa@gmail.com>
2026-01-21 11:01:24 +0700

    Make receiveAttack returns false
    Return false if the attack coordinate doesn't equal with any occupied coordinate of a ship.

--- src/functionality-tests/receiveAttack.spec.js
+++ src/functionality-tests/receiveAttack.spec.js
@@ -11,4 +11,10 @@ describe('receiveAttack method', () => {
         board.placeShip({x: 3, y: 4}, 3, Direction.HORIZONTAL);
         expect(board.receiveAttack({x: 3, y: 4})).toBe(true);
     })
+
+    test('return false if the attack coordinate hit the ship', () => {
+        const board = new GameBoard();
+        board.placeShip({x: 3, y: 4}, 3, Direction.VERTICAL);
+        expect(board.receiveAttack({x: 3, y: 3})).toBe(false);
+    })
 })
