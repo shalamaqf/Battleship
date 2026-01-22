b23f10b83f8a115d5f5180821ca416bb3aa3495f
Shalama <shalamafadillaa@gmail.com>
2026-01-22 15:53:56 +0700

    ADd logic for newGameSetUp function

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -2,5 +2,12 @@ import { Player } from '../functionality/player.js';
 import { Direction } from '../functionality/game-board.js';
 
 function newGameSetUp() {
-    
+    const realPlayer = new Player('You');
+    const computerPlayer = new Player('Computer');
+   
+    realPlayer.board.placeShip({x: 3, y: 3}, 3, Direction.HORIZONTAL);
+    realPlayer.board.placeShip({x: 7, y: 5}, 2, Direction.VERTICAL);
+
+    computerPlayer.board.placeShip({x: 7, y: 4}, 2, Direction.HORIZONTAL);
+    computerPlayer.board.placeShip({x: 2, y: 4}, 4, Direction.V);
 }
