2e1de8816b37af787f49c110e852511f78db908c
Shalama <shalamafadillaa@gmail.com>
2026-01-23 17:14:32 +0700

    Make computerAttack returns true
    Return true if computer attack an occupied coordinate that never been attacked.

--- src/functionality-tests/computerAttack.spec.js
+++ src/functionality-tests/computerAttack.spec.js
@@ -13,4 +13,11 @@ describe('computerAttack function', () => {
         computerAttack({x: 4, y: 5}, player.board);
         expect(computerAttack({x: 4, y: 5}, player.board)).toBe(false);
     })
+
+    test('return true if attack on coordinate that never been attacked', () => {
+        const player = new Player();
+        player.board.placeShip({x: 4, y: 5}, 3, Direction.HORIZONTAL);
+        computerAttack({x: 4, y: 5}, player.board);
+        expect(computerAttack({x: 6, y: 5}, player.board)).toBe(true);
+    })
 })
