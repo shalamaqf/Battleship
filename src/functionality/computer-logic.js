07e975c0619a37aaf52cacff30f3a988ed8b9c2e
Shalama <shalamafadillaa@gmail.com>
2026-01-23 16:50:42 +0700

    Create generateCoordinate function
    Create it to generate coordinate for computer to attack the opponent.

--- src/functionality/computer-logic.js
+++ src/functionality/computer-logic.js
@@ -1,3 +1,10 @@
 export function computerAttack() {
 
 }
+
+function generateCoordinate() {
+    const coordinateX = Math.floor(Math.random() * 8) + 1;
+    const coordinateY = Math.floor(Math.random() * 8) + 1;
+    const coordinate = {x: coordinateX, y: coordinateY};
+    return coordinate;
+}
