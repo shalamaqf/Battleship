1d6a01698d1f34de1cb3f43c0e136e8726f296d1
Shalama <shalamafadillaa@gmail.com>
2026-01-27 17:31:12 +0700

    Create private variables in gameUI module

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,3 +1,5 @@
 const gameUI = (function () {
-
+    const realPlayerBoardDOM = document.querySelector('.board.real-player');
+    const computerBoardDOM = document.querySelector('.board.computer-player');
+    const playerTurn = document.getElementById('player-turn');
 })();
