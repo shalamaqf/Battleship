9b8f65c975f8e0562d44adad55d4f1857102c2c1
Shalama <shalamafadillaa@gmail.com>
2026-01-27 14:56:10 +0700

    Create setupGame function in gameController
    Create the function to predetermined place the ships for players.

--- src/ui/game-controller.js
+++ src/ui/game-controller.js
@@ -1,6 +1,21 @@
+import { Direction } from "../functionality/game-board.js";
+import { Player } from "../functionality/player.js";
+
 const gameController = ( function () {
     const realPlayer = new Player('You');
     const computerPlayer = new Player('Computer');
     let currentPlayer;
     let gameOver = false;
+
+    function setupGame() {
+        realPlayer.board.placeShip({x: 2, y: 4}, 3, Direction.HORIZONTAL);
+        realPlayer.board.placeShip({x: 7, y: 5}, 2, Direction.VERTICAL);
+
+        computerPlayer.board.placeShip({x: 4, y: 7}, 2, Direction.HORIZONTAL);
+        computerPlayer.board.placeShip({x: 5, y: 1}, 3, Direction.VERTICAL);
+    }
+
+    return {
+        setupGame: setupGame,
+    }
 })();
