962f40d21b4fa51d3f4b4977bb8147b4539c9ba4
Shalama <shalamafadillaa@gmail.com>
2026-01-27 14:47:22 +0700

    Create private variables in gameController module

--- src/ui/game-controller.js
+++ src/ui/game-controller.js
@@ -1,3 +1,6 @@
 const gameController = ( function () {
-
+    const realPlayer = new Player('You');
+    const computerPlayer = new Player('Computer');
+    let currentPlayer;
+    let gameOver = false;
 })();
