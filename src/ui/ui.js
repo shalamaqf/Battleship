2cde071f2ffba7bdc6e05dc2de5b7aefabdc08c1
Shalama <shalamafadillaa@gmail.com>
2026-01-28 10:10:12 +0700

    Create createButtons function

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -2,4 +2,18 @@ const gameUI = (function () {
     const realPlayerBoardDOM = document.querySelector('.board.real-player');
     const computerBoardDOM = document.querySelector('.board.computer-player');
     const playerTurn = document.getElementById('player-turn');
+
+    function createButtons(boardDOM) {
+        for (let i = 8; i > 0; i--) {
+            for (let j = 1; j < 9; j++) {
+                const button = document.createElement('button');
+                button.classList.add('coordinate-button');
+                button.dataset.x = j;
+                button.dataset.y = i;
+                button.type = 'button'
+                boardDOM.append(button);
+            }
+        }
+    }
+
 })();
