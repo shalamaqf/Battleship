2b8d816d59fa42884a90beeeb885950351d463ad
Shalama <shalamafadillaa@gmail.com>
2026-01-29 14:07:15 +0700

    Create init function

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,3 +1,17 @@
 import { gameController } from "./game-controller.js"; 
 import { gameUI } from "./ui.js";
 
+function init() {
+    gameController.setupGame();
+    gameUI.renderPlayersBoards();
+    gameUI.showPlayerTurn();
+    gameUI.handleBoardState();
+
+    if (!gameController.isHumanTurn()) {
+        setTimeout(() => {
+            gameUI.computerTurn();
+        }, 2000);
+    }
+}
+
+init();
