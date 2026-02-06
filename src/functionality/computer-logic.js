2e8240b04d95c9c843ecce1d3e78a975ba5376ee
Shalama <shalamafadillaa@gmail.com>
2026-02-06 14:11:25 +0700

    Delete computerAttack function and its test file

--- src/functionality/computer-logic.js
+++ src/functionality/computer-logic.js
@@ -1,6 +1,3 @@
-export function computerAttack({x, y}, playerBoard) {
-    return (playerBoard.receiveAttack({x: x, y: y}));
-}
 
 export function generateCoordinate() {
     const coordinateX = Math.floor(Math.random() * 10) + 1;
