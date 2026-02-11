4f02569afa363a6bcad660ff125418e5fe309c1b
Shalama <shalamafadillaa@gmail.com>
2026-02-11 20:49:00 +0700

    Return correct object value when success attack
    Return the correct object value and not switching player if the attack is success.

--- src/functionality-tests/gameControllerFunctions.spec.js
+++ src/functionality-tests/gameControllerFunctions.spec.js
@@ -52,4 +52,42 @@ describe('playTurn function', () => {
         expect(result_2.isShipSunk).toBe(false);
         expect(result_2.gameOver).toBe(false);
     })
+
+    test('test the object value and switch turn in success attack scenario', () => {
+        gameController.setupGame();
+        const realPlayerBoard = gameController.getRealPlayerBoard();
+        const computerPlayerBoard = gameController.getComputerPlayerBoard();
+        let currentPlayer;
+
+        // Setup
+        realPlayerBoard.resetBoard();
+        computerPlayerBoard.resetBoard();
+
+        const realPlayerShip = new Ship(4, Direction.VERTICAL);
+        const computerPlayerShip = new Ship(4, Direction.HORIZONTAL);
+
+        realPlayerBoard.placeShip({x: 1, y: 2}, realPlayerShip);
+        computerPlayerBoard.placeShip({x: 5, y: 4}, computerPlayerShip);
+
+        // Real player attack
+        const result_1 = gameController.playTurn({x: 7, y: 4}, computerPlayerBoard);
+        currentPlayer = gameController.getCurrentPlayer();
+
+        expect(currentPlayer.name).toBe('You');
+        expect(result_1.hit).toBe(true);
+        expect(result_1.isShipSunk).toBe(false);
+        expect(result_1.gameOver).toBe(false);
+
+        // Make miss attack to switch the player
+        gameController.playTurn({x: 4, y: 4}, computerPlayerBoard)
+
+        // Computer player attack
+        const result_2 = gameController.playTurn({x: 1, y: 5}, realPlayerBoard);
+        currentPlayer = gameController.getCurrentPlayer();
+
+        expect(currentPlayer.name).toBe('Computer');
+        expect(result_2.hit).toBe(true);
+        expect(result_2.isShipSunk).toBe(false);
+        expect(result_2.gameOver).toBe(false);
+    })
 })
