b38cbbb8fa4eb8f376d308de83963766056ee8d4
Shalama <shalamafadillaa@gmail.com>
2026-02-11 14:14:45 +0700

    Make the getRealPlayerBoard returns the board

--- src/functionality-tests/gameControllerGetters.spec.js
+++ src/functionality-tests/gameControllerGetters.spec.js
@@ -1,3 +1,4 @@
+const { GameBoard } = require("../functionality/game-board");
 const { gameController } = require("../functionality/game-controller");
 const { Player } = require("../functionality/player");
 
@@ -9,5 +10,16 @@ describe('getCurrentPlayer function', () => {
     test('return the current player', () => {
         gameController.setupGame();
         expect(gameController.getCurrentPlayer()).toBeInstanceOf(Player);
+    })
+})
+
+describe('getRealPlayerBoard function', () => {
+    test('getRealPlayerBoard is defined', () => {
+        expect(gameController.getRealPlayerBoard).toBeDefined();
+    })
+
+    test('return the current player', () => {
+        gameController.setupGame();
+        expect(gameController.getRealPlayerBoard()).toBeInstanceOf(GameBoard);
     })
 })
