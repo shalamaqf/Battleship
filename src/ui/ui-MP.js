40f994ecf31116887dfcd45499e84a8d0d5bdd7c
Shalama <shalamafadillaa@gmail.com>
2026-02-24 17:04:22 +0700

    Create containers for module scoped

--- src/ui/ui-MP.js
+++ src/ui/ui-MP.js
@@ -4,4 +4,17 @@ export const gameUIMultiPlayer = ( function () {
     const playerTurn = document.getElementById('player-turn');
     const firstPlayerBoardSection = document.querySelector('.player.real-player');
     const secondPlayerBoardSection = document.querySelector('.player.computer-player');
+
+    // Container module scoped
+    const infoButtonContainer = document.createElement('div');
+    infoButtonContainer.id = 'infoButton-container';
+
+    const firstPlayerButtonContainer = document.createElement('div');
+    const secondPlayerButtonContainer = document.createElement('div');
+
+    firstPlayerButtonContainer.classList.add('button-container first-player');
+    secondPlayerButtonContainer.classList.add('button-container second-player');
+    
+    firstPlayerBoardSection.append(firstPlayerButtonContainer);
+    secondPlayerBoardSection.append(secondPlayerButtonContainer);
 })
