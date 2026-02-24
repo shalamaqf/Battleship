a60cd682fdda7137d1fcc9babc963a43a1149d7a
Shalama <shalamafadillaa@gmail.com>
2026-02-24 16:57:01 +0700

    Declare private variables

--- src/ui/ui-MP.js
+++ src/ui/ui-MP.js
@@ -1,3 +1,7 @@
 export const gameUIMultiPlayer = ( function () {
-    
+    const firstPlayerBoardDOM = document.querySelector('.board.real-player');
+    const secondPlayerBoardDOM = document.querySelector('.board.computer-player');
+    const playerTurn = document.getElementById('player-turn');
+    const firstPlayerBoardSection = document.querySelector('.player.real-player');
+    const secondPlayerBoardSection = document.querySelector('.player.computer-player');
 })
