3d09126e349ec8b611f11ee0a62472bd5eac3ea8
Shalama <shalamafadillaa@gmail.com>
2026-02-25 13:57:45 +0700

    Move the UI functions for live game to new module

--- src/ui/ui-MP.js
+++ src/ui/ui-MP.js
@@ -34,61 +34,6 @@ export const setupUIMultiPlayer = ( function () {
         }
     }
 
-    function updateButtonDOM(hit, button) {
-        if (hit === true) {
-            button.textContent = 'HIT';
-            button.classList.add('hit');
-            button.dataset.clicked = true;
-            button.disabled = true;
-        } else {
-            button.textContent = 'X';
-            button.classList.add('miss');
-            button.dataset.clicked = true;
-            button.disabled = true;
-        }
-    }
-
-    function handleFirstPlayerClick(button) {
-        button.addEventListener('click', () => {
-            const {x, y} = {x: parseInt(button.dataset.x), y: parseInt(button.dataset.y)};
-            const result = gameControllerMP.firstPlayerTurn({x, y});
-            reactOnTurn(result, button);
-            handleGameOver();
-        })
-    }
-
-    function handleSecondPlayerClick(button) {
-        button.addEventListener('click', () => {
-            const {x, y} = {x: parseInt(button.dataset.x), y: parseInt(button.dataset.y)};
-            const result = gameControllerMP.secondPlayerTurn({x, y});
-            reactOnTurn(result, button);
-            handleGameOver();
-        })
-    }
-
-    function reactOnTurn(result, button) {
-        deleteShowShipSunk();
-        updateBoard(result, button);
-        showPlayerTurn();
-        showShipSunk(result);
-    }
-
-    function updateBoard(result, button) {
-        const hit = result.hit;
-        updateButtonDOM(hit, button);
-
-        let currentPlayer = gameControllerMP.getCurrentPlayer();
-        const firstPlayer = gameControllerMP.getFirstPlayer();
-
-        if (currentPlayer === firstPlayer) {
-            enableBoard(secondPlayerBoardDOM);
-            disableBoard(firstPlayerBoardDOM);
-        } else {
-            enableBoard(firstPlayerBoardDOM);
-            disableBoard(secondPlayerBoardDOM);
-        }
-    }
-
     function disableBoard(boardDOM) {
         const buttons = boardElement.querySelectorAll('.coordinate-button');
         buttons.forEach(button => {
@@ -107,63 +52,9 @@ export const setupUIMultiPlayer = ( function () {
         });
     }
 
-    function handleGameOver() {
-        if (gameControllerMP.isGameOver()) {
-            deleteShowShipSunk();
-            disableBoard(firstPlayerBoardDOM);
-            disableBoard(secondPlayerBoardDOM);
-            showWinner();
-        }
-    }
-
     function renderPlayersBoards() {
         createButtons(firstPlayerBoardDOM);
         createButtons(secondPlayerBoardDOM);
-    }
-
-    function attachEventClickPlayerBoards() {
-        const buttonsFP = firstPlayerBoardDOM.querySelectorAll('.coordinate-button');
-        const buttonsSP = secondPlayerBoardDOM.querySelectorAll('.coordinate-button');
-
-        buttonsFP.forEach(button => {
-            handleSecondPlayerClick(button);
-        });
-
-        buttonsSP.forEach(button => {
-            handleFirstPlayerClick(button);
-        })
-    }
-
-    function showPlayerTurn() {
-        const currentPlayer = gameControllerMP.getCurrentPlayer();
-        playerTurn.textContent = currentPlayer.name;
-    }
-
-    function showWinner() {
-        const winner = gameControllerMP.getWinner();
-        playerTurn.textContent = 'The winner is ' +  winner.name + '!';
-    }
-
-    function showShipSunk(result) {
-        const shipSunkInfo = document.getElementById('ship-sunk');
-        const currentPlayer = gameControllerMP.getCurrentPlayer();
-        const firstPlayer = gameControllerMP.getFirstPlayer();
-        const secondPlayer = gameControllerMP.getSecondPlayer();
-
-        if (result.isShipSunk) {
-            if (currentPlayer === firstPlayer) {
-                shipSunkInfo.textContent = secondPlayer.name + "'s ship is sunk!";
-            } else {
-                shipSunkInfo.textContent = firstPlayer.name + "'s ship is sunk!";
-            }
-        } else {
-            return;
-        }
-    }
-
-    function deleteShowShipSunk() {
-        const shipSunkInfo = document.getElementById('ship-sunk');
-        shipSunkInfo.textContent = '';
     }
 
     function createRandomizeButton(container) {
@@ -349,5 +240,114 @@ export const setupUIMultiPlayer = ( function () {
 
 
 export const gameUIMultiPlayer = ( function () {
+    function updateButtonDOM(hit, button) {
+        if (hit === true) {
+            button.textContent = 'HIT';
+            button.classList.add('hit');
+            button.dataset.clicked = true;
+            button.disabled = true;
+        } else {
+            button.textContent = 'X';
+            button.classList.add('miss');
+            button.dataset.clicked = true;
+            button.disabled = true;
+        }
+    }
+
+    function handleFirstPlayerClick(button) {
+        button.addEventListener('click', () => {
+            const {x, y} = {x: parseInt(button.dataset.x), y: parseInt(button.dataset.y)};
+            const result = gameControllerMP.firstPlayerTurn({x, y});
+            reactOnTurn(result, button);
+            handleGameOver();
+        })
+    }
+
+    function handleSecondPlayerClick(button) {
+        button.addEventListener('click', () => {
+            const {x, y} = {x: parseInt(button.dataset.x), y: parseInt(button.dataset.y)};
+            const result = gameControllerMP.secondPlayerTurn({x, y});
+            reactOnTurn(result, button);
+            handleGameOver();
+        })
+    }
+
+    function reactOnTurn(result, button) {
+        deleteShowShipSunk();
+        updateBoard(result, button);
+        showPlayerTurn();
+        showShipSunk(result);
+    }
+
+    function updateBoard(result, button) {
+        const hit = result.hit;
+        updateButtonDOM(hit, button);
+
+        let currentPlayer = gameControllerMP.getCurrentPlayer();
+        const firstPlayer = gameControllerMP.getFirstPlayer();
+
+        if (currentPlayer === firstPlayer) {
+            enableBoard(secondPlayerBoardDOM);
+            disableBoard(firstPlayerBoardDOM);
+        } else {
+            enableBoard(firstPlayerBoardDOM);
+            disableBoard(secondPlayerBoardDOM);
+        }
+    }
+
+    function attachEventClickPlayerBoards() {
+        const buttonsFP = firstPlayerBoardDOM.querySelectorAll('.coordinate-button');
+        const buttonsSP = secondPlayerBoardDOM.querySelectorAll('.coordinate-button');
+
+        buttonsFP.forEach(button => {
+            handleSecondPlayerClick(button);
+        });
+
+        buttonsSP.forEach(button => {
+            handleFirstPlayerClick(button);
+        })
+    }
+
+
+    function handleGameOver() {
+        if (gameControllerMP.isGameOver()) {
+            deleteShowShipSunk();
+            disableBoard(firstPlayerBoardDOM);
+            disableBoard(secondPlayerBoardDOM);
+            showWinner();
+        }
+    }
+
+    function showPlayerTurn() {
+        const currentPlayer = gameControllerMP.getCurrentPlayer();
+        playerTurn.textContent = currentPlayer.name;
+    }
+
+    function showWinner() {
+        const winner = gameControllerMP.getWinner();
+        playerTurn.textContent = 'The winner is ' +  winner.name + '!';
+    }
+
+    function showShipSunk(result) {
+        const shipSunkInfo = document.getElementById('ship-sunk');
+        const currentPlayer = gameControllerMP.getCurrentPlayer();
+        const firstPlayer = gameControllerMP.getFirstPlayer();
+        const secondPlayer = gameControllerMP.getSecondPlayer();
+
+        if (result.isShipSunk) {
+            if (currentPlayer === firstPlayer) {
+                shipSunkInfo.textContent = secondPlayer.name + "'s ship is sunk!";
+            } else {
+                shipSunkInfo.textContent = firstPlayer.name + "'s ship is sunk!";
+            }
+        } else {
+            return;
+        }
+    }
+
+    function deleteShowShipSunk() {
+        const shipSunkInfo = document.getElementById('ship-sunk');
+        shipSunkInfo.textContent = '';
+    }
 
 })();
