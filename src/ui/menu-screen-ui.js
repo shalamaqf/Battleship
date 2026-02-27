8e34471a6237fa253cdb629663ebf57f88699722
Shalama <shalamafadillaa@gmail.com>
2026-02-27 13:07:56 +0700

    Create createText function

--- src/ui/menu-screen-ui.js
+++ src/ui/menu-screen-ui.js
@@ -2,4 +2,11 @@ import { inputPlayerNameUI } from "./ui-MP.js";
 
 export const menuScreenUI = ( function () {
     const main = document.querySelector('main');
+
+    function createText(textClass) {
+        const p = document.createElement('p');
+        p.classList.add(textClass);
+
+        return p;
+    }
 })();
